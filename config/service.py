43b30b41b81c64e19d0c2328dc051df0c9e12fab
Richard Jones <richard@cottagelabs.com>
2015-07-02 11:28:06 +0100

    first bindings for jper api client

--- config/service.py
+++ config/service.py
@@ -24,3 +24,12 @@ ELASTIC_SEARCH_MAPPINGS = [
 
 ACCOUNT_ENABLE = False
 SECRET_KEY = "super-secret-key"
+
+############################################
+## JPER Client information
+
+# Base URL for requests to the JPER API
+JPER_BASE_URL = "http://router.jisc.ac.uk/api/v1"
+
+# API key to use for authenticated requests against JPER API
+JPER_API_KEY = ""
